476d6a6db6121f7daa127f55eb5fec78a26aae6e
Shamee <127168168+s-morency@users.noreply.github.com>
2023-06-27 14:41:10 -0400

    css bonjour

--- includes/header.php
+++ includes/header.php
@@ -5,13 +5,19 @@
             <span class="header-text">Foodie Share</span>
         </a>
     </div>
+    <?php
+    if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) : ?>
+   
+    <?php else : ?>
+    <span class = "header-greeting">Bonjour <?= $_SESSION['username'] ?> </span>    
+    <?php endif; ?>
     <nav class="header-nav">
         <a class="header-button" href="../search.php">Recherche</a>
         <?php
             if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) : ?>
                 <a class="header-button" href="../login.php">Connexion</a>
             <?php else : ?>
-                <a class="header-button" href="../profile.php?userId=<?= $_SESSION['userId'] ?>">Bonjour <?= $_SESSION['username'] ?></a>
+            <a class="header-button" href="../profile.php?userId=<?= $_SESSION['userId'] ?>">Votre profil</a>
             <?php endif; ?>
     </nav>
 </header>
